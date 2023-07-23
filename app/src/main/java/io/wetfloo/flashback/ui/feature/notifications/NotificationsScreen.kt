7a0377a8a2cc412a2914dea9127f10f1e76bb8fe
wetfloo <yahahaha.1234567890@gmail.com>
2023-07-23 11:08:12 +0700

    Move content out, make some previews

--- app/src/main/java/io/wetfloo/flashback/ui/feature/notifications/NotificationsScreen.kt
+++ app/src/main/java/io/wetfloo/flashback/ui/feature/notifications/NotificationsScreen.kt
@@ -1,51 +1,39 @@
 package io.wetfloo.flashback.ui.feature.notifications
 
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import io.wetfloo.flashback.ui.nav.PagedNavGraph
+import io.wetfloo.flashback.ui.theme.AppTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Destination
-@PagedNavGraph(start = true)
-fun NotificationsScreen(
-    navigator: DestinationsNavigator,
-    viewModel: NotificationsViewModel = hiltViewModel(),
+private fun NotificationScreenContent(
+    state: List<String>,
 ) {
     val context = LocalContext.current
 
-    val list by viewModel
-        .itemsState
-        .collectAsStateWithLifecycle()
-
-    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
-
-    Scaffold(
-        modifier = Modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection),
-    ) { scaffoldPaddingValues ->
+    Scaffold { scaffoldPaddingValues ->
         LazyColumn(
             contentPadding = scaffoldPaddingValues,
             modifier = Modifier
                 .fillMaxSize(),
         ) {
-            items(items = list) { item ->
+            items(items = state) { item ->
                 Text(
                     text = item,
                     modifier = Modifier
@@ -57,5 +45,31 @@ fun NotificationsScreen(
                 )
             }
         }
+    }
+}
+
+@Composable
+@Destination
+@PagedNavGraph(start = true)
+fun NotificationsScreen(
+    navigator: DestinationsNavigator,
+    viewModel: NotificationsViewModel = hiltViewModel(),
+) {
+    val list by viewModel
+        .itemsState
+        .collectAsStateWithLifecycle()
+
+    NotificationScreenContent(state = list)
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun NotificationsScreenPreview() {
+    AppTheme {
+        val items = remember {
+            (0..100).map { "Item $it" }
+        }
+        NotificationScreenContent(state = items)
     }
 }
