ecdc8f30b0c90682310b0ac55fc09f83c6798d47
wetfloo <yahahaha.1234567890@gmail.com>
2023-07-23 15:39:18 +0700

    Work on notifications screen

--- app/src/main/java/io/wetfloo/flashback/ui/feature/notifications/NotificationsScreen.kt
+++ app/src/main/java/io/wetfloo/flashback/ui/feature/notifications/NotificationsScreen.kt
@@ -1,23 +1,32 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package io.wetfloo.flashback.ui.feature.notifications
 
 import android.content.Intent
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import io.wetfloo.flashback.R
 import io.wetfloo.flashback.domain.model.NotificationDomain
 import io.wetfloo.flashback.ui.nav.PagedNavGraph
 import io.wetfloo.flashback.ui.theme.AppTheme
@@ -31,22 +40,35 @@ private fun NotificationsScreenContent(
 ) {
     val context = LocalContext.current
 
-    Scaffold { scaffoldPaddingValues ->
-        LazyColumn(
-            contentPadding = scaffoldPaddingValues,
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(stringResource(R.string.notifications_title))
+                },
+            )
+        },
+    ) { scaffoldPaddingValues ->
+        Column(
             modifier = Modifier
-                .fillMaxSize(),
+                .padding(top = 16.dp),
         ) {
-            items(items = state) { item ->
-                Text(
-                    text = item.content,
-                    modifier = Modifier
-                        .clickable {
-                            context.startActivity(
-                                Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"),
-                            )
-                        }
-                )
+            LazyColumn(
+                contentPadding = scaffoldPaddingValues,
+                modifier = Modifier
+                    .fillMaxSize(),
+            ) {
+                items(items = state) { item ->
+                    Text(
+                        text = item.content,
+                        modifier = Modifier
+                            .clickable {
+                                context.startActivity(
+                                    Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"),
+                                )
+                            }
+                    )
+                }
             }
         }
     }
